80ec983b292906f62d72a765ec627472f9eeb8b4
Kristof Keppens <kkeppens@gmail.com>
2012-11-20 16:29:26 +0100

    start work on matterhorn post install script for fpm

---
+++ post-install.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e
+
+MATTERHORN_HOME=/opt/matterhorn
+MATTERHORN_USER=matterhorn
+MATTERHORN_GROUP=matterhorn
+
+
+case "$1" in
+    configure)
+        #start opencast matterhorn on boot
+        update-rc.d matterhorn defaults 99
+
+        #Create matterhorn group if not exists
+        if ! getent group matterhorn > /dev/null; then
+            groupadd matterhorn
+        fi
+
+        #Create matterhorn user if it not exists
+        if ! getent passwd matterhorn > /dev/null; then
+            adduser --home $MATTERHORN_HOME \
+                    --system \
+                    --group $MATTERHORN_GROUP \
+                    --no-create-home \
+                    --disabled-password \
+                    --quiet $MATTERHORN_USER \
+        fi
+    ;;
+esac
+
