6ef5a6784a5e317c7b54fb3228d4a49ad69322ec
Kristof Keppens <kkeppens@gmail.com>
2012-11-26 14:06:06 +0100

    include pid directory and remove cache dir

--- opencast-package.sh
+++ opencast-package.sh
@@ -1,8 +1,8 @@
 #!/bin/sh 
 
 NAME=opencast-matterhorn
-VERSION=1.4.0-r13262
+VERSION=1.4.0-r13262-4
 
 fpm -s dir -t deb -n $NAME -v $VERSION -d "openjdk-6-jre" -m "Kristof Keppens <k.keppens@ugent.be>" \
     --description "Opencast Matterhorn" --post-install post-install.sh \
-    /opt/matterhorn /etc/init.d/matterhorn /var/log/matterhorn
+    /opt/matterhorn /etc/init.d/matterhorn /var/log/matterhorn /var/run/matterhorn
