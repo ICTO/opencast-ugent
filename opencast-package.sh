ff198a4edb870c9efc466ec3f8ef6fb3b756f64f
Kristof Keppens <kkeppens@gmail.com>
2012-11-26 14:34:38 +0100

    add pre install and pre remove script to package

--- opencast-package.sh
+++ opencast-package.sh
@@ -5,4 +5,5 @@ VERSION=1.4.0-r13262-4
 
 fpm -s dir -t deb -n $NAME -v $VERSION -d "openjdk-6-jre" -m "Kristof Keppens <k.keppens@ugent.be>" \
     --description "Opencast Matterhorn" --post-install post-install.sh \
+    --pre-install pre-install.sh --pre-remove pre-remove.sh \
     /opt/matterhorn /etc/init.d/matterhorn /var/log/matterhorn /var/run/matterhorn
