a54cb65941dbdb98cd184fc46d0a5d90e58dff19
Kristof Keppens <kkeppens@gmail.com>
2012-11-26 11:05:28 +0100

    fpm based package script for opencast matterhorn

---
+++ opencast-package.sh
@@ -0,0 +1,8 @@
+#!/bin/sh 
+
+NAME=opencast-matterhorn
+VERSION=1.4.0-r13262
+
+fpm -s dir -t deb -n $NAME -v $VERSION -d "openjdk-6-jre" -m "Kristof Keppens <k.keppens@ugent.be>" \
+    --description "Opencast Matterhorn" --post-install post-install.sh \
+    /opt/matterhorn /etc/init.d/matterhorn /var/log/matterhorn
