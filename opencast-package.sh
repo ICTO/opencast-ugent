689d33750b0613281f1fed2eedc12ea71aab69e3
Kristof Keppens <kkeppens@gmail.com>
2012-11-26 14:46:19 +0100

    add post uninstall script

--- opencast-package.sh
+++ opencast-package.sh
@@ -5,5 +5,5 @@ VERSION=1.4.0-r13262-6
 
 fpm -s dir -t deb -n $NAME -v $VERSION -d "openjdk-6-jre" -m "Kristof Keppens <k.keppens@ugent.be>" \
     --description "Opencast Matterhorn" --post-install post-install.sh \
-    --pre-install pre-install.sh --pre-remove pre-remove.sh \
+    --pre-install pre-install.sh --pre-uninstall pre-remove.sh --post-uninstall post-remove.sh \
     /opt/matterhorn /etc/init.d/matterhorn /var/log/matterhorn /var/run/matterhorn
